6069833c129674f5cb008eade3f59e9ad6c63419
air-awan <airawan@gmail.com>
2014-11-18 16:03:36 -0800

    Solution to Programming Assignment Week 2 First submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are working together to return inverse matrix value
+## To use:
+## 1. Call makeCacheMatrix with square matrix as input, assign the output to an object
+## 2. Call cacheSolve with result of no 1 as input
+## 3. When you need too recall the result, just run no. 2 again
 
-## Write a short comment describing this function
+## This function will return a list which contains function:
+## set: set the matrix with value from input
+## get: get the matrix value
+## setsolve: set the solve (inverse matrix) value
+## getsolve: get the solve (inverse matrix) value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function () m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
+## This function will return inverse matrix value
+## from cache with message "getting cached data"
+## or from calculation without message
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)        
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setsolve(m)
+    m
 }
