c0fd7a1f34ffa6609f3f1f2d37aff9a7c34f54f9
air-awan <airawan@gmail.com>
2014-11-19 16:48:24 -0800

    Fix typo errors and change in variable names.

--- cachematrix.R
+++ cachematrix.R
@@ -2,40 +2,41 @@
 ## To use:
 ## 1. Call makeCacheMatrix with square matrix as input, assign the output to an object
 ## 2. Call cacheSolve with result of no 1 as input
-## 3. When you need too recall the result, just run no. 2 again
+## 3. When you need to recall the result, just run no. 2 again
 
 ## This function will return a list which contains function:
 ## set: set the matrix with value from input
 ## get: get the matrix value
-## setsolve: set the solve (inverse matrix) value
-## getsolve: get the solve (inverse matrix) value
+## setInverse: set the cacheInverse (inverse matrix) value
+## getInverse: get the cacheInverse (inverse matrix) value
 
-makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+makeCacheMatrix <- function(cacheMatrix = matrix()) {
+    cacheInverse <- NULL
     set <- function(y) {
-        x <<- y
-        m <<- NULL
+        cacheMatrix <<- y
+        cacheInverse <<- NULL
     }
-    get <- function() x
-    setsolve <- function(solve) m <<- solve
-    getsolve <- function () m
+    get <- function() cacheMatrix
+    setInverse <- function(solve) cacheInverse <<- solve
+    getInverse <- function () cacheInverse
     list(set = set, get = get,
-         setsolve = setsolve,
-         getsolve = getsolve)
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 ## This function will return inverse matrix value
-## from cache with message "getting cached data"
-## or from calculation without message
+## from cache if available, marked with message "Cached Inverse"
+## or from calculation with message "Calculated Inverse"
 
-cacheSolve <- function(x) {
-    m <- x$getsolve()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)        
+cacheSolve <- function(cache) {
+    inverse <- cache$getInverse()
+    if(!is.null(inverse)) {
+        message("Cached Inverse")
+        return(inverse)        
     }
-    data <- x$get()
-    m <- solve(data)
-    x$setsolve(m)
-    m
+    data <- cache$get()
+    inverse <- solve(data)
+    cache$setInverse(inverse)
+    message("Calculated Inverse")
+    inverse
 }
